43402fae64fb9be84ecb383ea9489de700a9c7ee
Veteroch4k <indefinite05@mail.ru>
2025-12-07 15:14:06 +0700

    feat: add authentication

---
+++ src/main/java/com/popov314/autoparts/config/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.popov314.autoparts.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
+public class SecurityConfiguration {
+
+  private final PasswordEncoder pwEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+  @Bean
+  UserDetailsService authenticayion() {
+    UserDetails director = User.builder()
+        .username("director")
+        .password(pwEncoder.encode("director"))
+        .roles("USER", "ADMIN")
+        .build();
+
+    return new InMemoryUserDetailsManager(director);
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    http
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/public/**").permitAll()
+            .anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults());
+
+    return http.build();
+
+  }
+
+
+}

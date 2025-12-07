5072bd13e88076febc879c0dad0a9f1998c52b68
Veteroch4k <indefinite05@mail.ru>
2025-12-07 18:49:17 +0700

    feat: add React frontend

--- src/main/java/com/popov314/autoparts/config/SecurityConfiguration.java
+++ src/main/java/com/popov314/autoparts/config/SecurityConfiguration.java
@@ -1,8 +1,8 @@
 package com.popov314.autoparts.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfiguration {
